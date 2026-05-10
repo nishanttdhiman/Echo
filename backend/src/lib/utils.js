db0c9553c5d747442b2f30ae102296942f8ab65b
nishanttdhiman <nishantdhiman0209@gmail.com>
2026-05-10 10:41:10 +0530

    fix production cookies

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -10,14 +10,12 @@ export const generateToken = (userId, res) => {
   const token = jwt.sign({ userId }, JWT_SECRET, {
     expiresIn: "7d",
   });
-
   res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true,
-    sameSite: ENV.NODE_ENV === "production" ? "none" : "strict",
-    secure: ENV.NODE_ENV === "production",
+    secure: true,
+    sameSite: "none",
   });
-  return token;
 };
 
 // http://localhost
